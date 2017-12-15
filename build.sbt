b3f3366babbf0f38dcd1a987ea554e336b55a090
Colin Woodbury <colingw@gmail.com>
2017-12-15 09:25:50 -0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,27 @@
+name := """gt-stitch"""
+
+version := "1.0.0"
+
+scalaVersion in ThisBuild := "2.11.12"
+
+resolvers += "locationtech-releases" at "https://repo.locationtech.org/content/repositories/releases/"
+
+scalacOptions := Seq(
+  "-deprecation",
+  "-Ypartial-unification",
+  "-Ywarn-value-discard",
+  "-Ywarn-unused-import",
+  "-Ywarn-dead-code",
+  "-Ywarn-numeric-widen"
+)
+
+fork in run := true
+
+libraryDependencies ++= Seq(
+  "org.locationtech.geotrellis" %% "geotrellis-spark"  % "1.2.0",
+  "org.locationtech.geotrellis" %% "geotrellis-s3"     % "1.2.0",
+  "org.apache.spark"            %% "spark-core"        % "2.2.0",
+  "com.monovore"                %% "decline"           % "0.4.0-RC1",
+  "com.monovore"                %% "decline-refined"   % "0.4.0-RC1",
+  "org.typelevel"               %% "cats-core"         % "1.0.0-RC1"
+)
---
+++ src/main/scala/geotrellis/Stitch.scala
@@ -0,0 +1,64 @@
+package geotrellis
+
+import cats.implicits._
+import com.monovore.decline._
+import com.monovore.decline.refined._
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric.Positive
+import geotrellis.raster._
+import geotrellis.raster.io.geotiff._
+import geotrellis.spark._
+import geotrellis.spark.io._
+import geotrellis.spark.io.kryo._
+import geotrellis.spark.io.s3._
+import org.apache.spark._
+import org.apache.spark.serializer.KryoSerializer
+
+/*
+ run --bucket "rasterfoundry-staging-catalogs-us-east-1" --prefix "layers" --layer "e47d53d6-724c-4760-8fe3-07d85e8efc55" --zoom 7
+ */
+
+object Stitch extends CommandApp(
+
+  name   = "gt-stitch",
+  header = "Turn a GeoTrellis layer into a GeoTiff",
+  main   = {
+
+    /* Ensures that only positive, non-zero values can be given as arguments. */
+    type UInt = Int Refined Positive
+
+    val bucketO: Opts[String] = Opts.option[String]("bucket", help = "S3 bucket.")
+    val prefixO: Opts[String] = Opts.option[String]("prefix", help = "Path to GeoTrellis layer catalog.")
+    val layerO:  Opts[String] = Opts.option[String]("layer",  help = "Name of the layer.")
+    val zoomO:   Opts[UInt]   = Opts.option[UInt]("zoom",     help = "Zoom level of the layer to stitch.")
+    val tiffO:   Opts[String] = Opts.option[String]("tiff",   help = "Name of the TIFF to be output.").withDefault("stiched.tiff")
+
+    (bucketO, prefixO, layerO, zoomO, tiffO).mapN { (bucket, prefix, layer, zoom, tiff) =>
+
+      val conf = new SparkConf()
+        .setIfMissing("spark.master", "local[*]")
+        .setAppName("GeoTrellis Stitch")
+        .set("spark.serializer", classOf[KryoSerializer].getName)
+        .set("spark.kryo.registrator", classOf[KryoRegistrator].getName)
+
+      implicit val sc = new SparkContext(conf)
+
+      val reader = S3LayerReader(S3AttributeStore(bucket, prefix))
+
+      val lid = LayerId(layer, zoom.value)
+
+      val tiles: MultibandTileLayerRDD[SpatialKey] =
+        reader.read[SpatialKey, MultibandTile, TileLayerMetadata[SpatialKey]](lid)
+
+      val tile: Raster[MultibandTile] = tiles.stitch
+
+      val geotiff: MultibandGeoTiff = MultibandGeoTiff(tile, tiles.metadata.crs)
+
+      geotiff.write(tiff)
+
+      sc.stop()
+
+    }
+  }
+)
